a9c0299fb8319afdb33069b8e109c7441348c191
Michał Kuwik <Michal7074@poczta.fm>
2019-05-01 00:03:50 +0200

    connect on the fetch with my API in function searchText

--- js/main.js
+++ js/main.js
@@ -6,7 +6,21 @@ const searchCities = async searchText => {
     const res = await fetch('../data/states.json');
     const cities = await res.json();
 
-    console.log(cities);
-}
+    //get matches to current text input
+    let matches = cities.filter(city => {
+        const regex = new RegExp(`${searchText}`, 'gi');
+        return city.name.match(regex);
+    });
+
+    //if text in input will be empty
+    if(searchText.length === 0){
+        matches = [];
+    }
+    
+    //function to watch our score
+    outputHtml(matches);
+};
+
+//show results in HTML
 
 searchInput.addEventListener('input', () => searchCities(search.value));
